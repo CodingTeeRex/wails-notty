7985c69384872539ef78d51defae11d86db16771
ivanpjh <ivanpjh@gmail.com>
2025-03-09 19:49:12 +0800

    window settings

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -23,10 +25,19 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 0, G: 0, B: 0, A: 230},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
+		},
+		Linux: &linux.Options{
+			WindowIsTranslucent: true,
+		},
+		Windows: &windows.Options{
+			WindowIsTranslucent: true,
+			DisableWindowIcon:   true,
+			Theme:               windows.SystemDefault,
+			BackdropType:        windows.Mica,
 		},
 	})
 
